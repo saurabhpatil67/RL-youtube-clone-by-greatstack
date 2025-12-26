471b28432eb3c2522d8d971ac1621afe1c2e2dce
saurabhpatil67 <saurab.nik2002@gmail.com>
2025-12-26 23:16:54 +0530

    feat: feed and playvideo component code complete with  style

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Home.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import Feed from '../../Components/Feed/Feed'
 
 
 
@@ -8,6 +9,9 @@ const Home = ({sidebar}) => {
   return (
     <>
       <Sidebar sidebar={sidebar}/>
+      <div className={`container ${sidebar?"":'large-container'}`}>
+          <Feed/>
+      </div>
     </>
   )
 }
