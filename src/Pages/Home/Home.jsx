316339f5c392932723e3d71a45dd57ece92514ea
saurabhpatil67 <saurab.nik2002@gmail.com>
2025-12-27 22:30:40 +0530

    feat: recommended section complete wirh style and little bit fetch api code write

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import Feed from '../../Components/Feed/Feed'
@@ -6,11 +6,17 @@ import Feed from '../../Components/Feed/Feed'
 
 
 const Home = ({sidebar}) => {
+
+
+  const [category, setCategory] = useState(0);
+
+
+
   return (
     <>
-      <Sidebar sidebar={sidebar}/>
+      <Sidebar category={category} setCategory={setCategory}   sidebar={sidebar}/>
       <div className={`container ${sidebar?"":'large-container'}`}>
-          <Feed/>
+          <Feed category={category}/>
       </div>
     </>
   )
