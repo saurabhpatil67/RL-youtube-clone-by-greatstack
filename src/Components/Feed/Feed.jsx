db61c41ab8f144a87c8d308aa76b9e50add204d3
saurabhpatil67 <saurab.nik2002@gmail.com>
2025-12-28 22:00:32 +0530

    feat: In feed section real data fetch from API

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Feed.css'
 import thumbnail1 from '../../assets/thumbnail1.png'
 import thumbnail2 from '../../assets/thumbnail2.png'
@@ -8,114 +8,41 @@ import thumbnail5 from '../../assets/thumbnail5.png'
 import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
-import { Link } from 'react-router-dom'
-import {API_KEY} from '../../data'
-const Feed = () => {
+import { data, Link } from 'react-router-dom'
+import {API_KEY, value_converter} from '../../data'
+import moment from 'moment'
 
+
+const Feed = ({category}) => {
+
+  const [data, setData] = useState([]);
   const fetchData = async () => {
+
+
+
       const videoList_url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
+      await fetch(videoList_url).then(response => response.json()).then(data =>setData(data.items))
   }
 
 
+  useEffect(() => {
+      fetchData();    
+  }, [category])
+  
+
   return ( 
     <div className="feed">
-      <Link to={'/video/20/4532'} className='card'>
-        <img src={thumbnail1} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
+      {data.map((items,index) => {
+        return(
+      <Link key={index} to={'/video/${items.snippet.categoryId}/${items.id}'} className='card'>
+        <img src={items.snippet.thumbnails.medium.url} alt="" />
+        <h2>{items.snippet.title}</h2>
+        <h3>{items.snippet.channelTitle}</h3>
+        <p>{value_converter(items.statistics.viewCount)} views &bull; {moment(items.snippet.publishedAt).fromNow()} </p>
       </Link>
-      <div className='card'>
-        <img src={thumbnail2} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail3} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail4} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail5} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail6} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail7} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail8} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
 
-      <div className='card'>
-        <img src={thumbnail1} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail2} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail3} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail4} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail5} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail6} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3 >
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail7} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
-      <div className='card'>
-        <img src={thumbnail8} alt="" />
-        <h2>Best channel to learn coding that help you to be a web developer</h2>
-        <h3>GreatStack</h3>
-        <p>15k views &bull; 2 days ago </p>
-      </div>
+        )
+      })}
     </div>
   )
 }
--- src/data.js
+++ src/data.js
@@ -1,1 +1,13 @@
 export const API_KEY = ''
+
+export const value_converter = (value) => {
+       if (value>=100000) {
+         return(Math.floor(value/100000))+'M'
+       }
+       if(value>=1000){
+         return(Math.floor(value/1000))+'K'
+       }
+       else{
+        return(value)
+       }
+}
