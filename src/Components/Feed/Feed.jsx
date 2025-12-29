89f4a5ad16d7b5cdb0ed7c172cfd04a0918d51a0
saurabhpatil67 <saurab.nik2002@gmail.com>
2025-12-29 23:11:37 +0530

    format documment

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -9,37 +9,37 @@ import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
 import { data, Link } from 'react-router-dom'
-import {API_KEY, value_converter} from '../../data'
+import { API_KEY, value_converter } from '../../data'
 import moment from 'moment'
 
 
-const Feed = ({category}) => {
+const Feed = ({ category }) => {
 
   const [data, setData] = useState([]);
   const fetchData = async () => {
 
 
 
-      const videoList_url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
-      await fetch(videoList_url).then(response => response.json()).then(data =>setData(data.items))
+    const videoList_url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
+    await fetch(videoList_url).then(response => response.json()).then(data => setData(data.items))
   }
 
 
   useEffect(() => {
-      fetchData();    
+    fetchData();
   }, [category])
-  
 
-  return ( 
+
+  return (
     <div className="feed">
-      {data.map((items,index) => {
-        return(
-      <Link key={index} to={'/video/${items.snippet.categoryId}/${items.id}'} className='card'>
-        <img src={items.snippet.thumbnails.medium.url} alt="" />
-        <h2>{items.snippet.title}</h2>
-        <h3>{items.snippet.channelTitle}</h3>
-        <p>{value_converter(items.statistics.viewCount)} views &bull; {moment(items.snippet.publishedAt).fromNow()} </p>
-      </Link>
+      {data.map((items, index) => {
+        return (
+          <Link key={index} to={'/video/${items.snippet.categoryId}/${items.id}'} className='card'>
+            <img src={items.snippet.thumbnails.medium.url} alt="" />
+            <h2>{items.snippet.title}</h2>
+            <h3>{items.snippet.channelTitle}</h3>
+            <p>{value_converter(items.statistics.viewCount)} views &bull; {moment(items.snippet.publishedAt).fromNow()} </p>
+          </Link>
 
         )
       })}
